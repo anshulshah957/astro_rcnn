5e20c8e97e357b8c1fc4396d972b8760418b8765
Anshul <anshuls3@illinois.edu>
2020-08-26 12:13:02 -0500

    Added param initizalization

--- k-sersics/sersics.py
+++ k-sersics/sersics.py
@@ -1,10 +1,39 @@
+import os
 import numpy as np
+from astropy.io import fits
+'''
+Sersic distribution information
+https://en.wikipedia.org/wiki/Sersic_profile#
+'''
 
-def optim_params():
+def initialize_params(k, shape):
+    width = shape[0]
+    height = shape[1]
+    params = []
+
+    for i in range(0, k):
+        n = np.random.uniform(0, 100)
+        b = 2 * (n ** (-1/3))
+        a = np.random.uniform(0, 100)
+
+        params.append([n, b, a])
+
+    return params
+
+def optim_params(params, data):
     pass
 
-def optim_pixels():
+def optim_pixels(params, data):
     pass
 
-def k_sersics(k):
+def k_sersics(k, fits_fp):
+    data = 0 
+    with fits.open(fits_fp) as hdul:
+        data = hdul[0].data
+
+    shape = data.shape
+    params = initialize_params(k, shape)
+    
+    
+
     pass

19c233917e697a8435094f1d3601354ad24ef188
Anshul <anshuls3@illinois.edu>
2020-08-26 12:34:49 -0500

    Optim calls

--- k-sersics/sersics.py
+++ k-sersics/sersics.py
@@ -6,6 +6,8 @@ Sersic distribution information
 https://en.wikipedia.org/wiki/Sersic_profile#
 '''
 
+NUM_ITERS = 50
+
 def initialize_params(k, shape):
     width = shape[0]
     height = shape[1]
@@ -20,20 +22,27 @@ def initialize_params(k, shape):
 
     return params
 
-def optim_params(params, data):
+def optim_params(pixel_partition, data):
     pass
 
 def optim_pixels(params, data):
     pass
 
-def k_sersics(k, fits_fp):
+def k_sersics(k, fits_fp, num_iters = None):
     data = 0 
     with fits.open(fits_fp) as hdul:
         data = hdul[0].data
 
+    if not num_iters:
+        num_iters = NUM_ITERS
+
     shape = data.shape
     params = initialize_params(k, shape)
-    
-    
+    pixel_partition = 0
 
-    pass
+    for i in range(0, num_iters):
+        pixel_partition = optimize_pixels(params, data)
+        params = optimize_params(pixel_partition, data)
+
+    return params
+
